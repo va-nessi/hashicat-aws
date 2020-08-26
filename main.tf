e099f0c7b5b98c6058a34a28dbf85ad14a4201ff
va-nessi <49526329+va-nessi@users.noreply.github.com>
2020-08-26 15:31:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,8 +123,8 @@ resource aws_instance "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "${var.prefix}-hashicat-instance",
-    Billable = True,
+    Name = "${var.prefix}-hashicat-instance"
+    Billable = true
     Department = "devops"
   }
 }
